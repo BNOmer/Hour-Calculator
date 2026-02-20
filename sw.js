6891a3a3cd262182bf014155c252a1d1d60eb9a7
BNOmer <134260923+BNOmer@users.noreply.github.com>
2026-02-20 23:25:55 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+// This is a minimal service worker. 
+// It simply listens for network requests so Chrome registers the app as a PWA.
+self.addEventListener('fetch', function(event) {
+    // Leave empty for basic installation requirements
+});
